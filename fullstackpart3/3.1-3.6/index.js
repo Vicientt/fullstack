ffd871b2e79313a7c571bcc07166b582dbec847b
Vicientt <nguyen15@kenyon.edu>
2025-11-16 02:21:30 -0500

    Back + Front ready

--- fullstackpart3/3.1-3.6/index.js
+++ fullstackpart3/3.1-3.6/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+
+const cors = require('cors')
+app.use(cors())
 let persons = [
     { 
       "id": "1",
@@ -102,8 +105,10 @@ app.post('/api/persons',(request,response) => {
         console.log(persons)
     }
 })
+
+app.use(express.static('dist'))
 // --------------------------
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`)
 })
